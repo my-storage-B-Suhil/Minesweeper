7f2dacb0108b8c2747aa9c17941482069468db7d
my-storage-B-Suhil <suhil.mohmmed2004@gmail.com>
2025-01-02 13:37:36 +0300

    add node class and commit all classes

--- src/minesweeper/StartFrame.java
+++ src/minesweeper/StartFrame.java
@@ -1,17 +1,8 @@
 package minesweeper;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
 
 public class StartFrame extends JFrame implements ActionListener {
     StartFrame(){
